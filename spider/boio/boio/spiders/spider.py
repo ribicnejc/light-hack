c4d56f644ebd468f077c3049dac2787c5c0425ba
Nejc Ribic <nejc.ribic95@gmail.com>
2021-05-06 14:01:57 +0200

    Added spider structure

---
+++ spider/boio/boio/spiders/spider.py
@@ -0,0 +1,10 @@
+import scrapy
+
+
+class SpiderSpider(scrapy.Spider):
+    name = 'spider'
+    allowed_domains = ['spider.com']
+    start_urls = ['http://spider.com/']
+
+    def parse(self, response):
+        pass
