50ad6040eb560ce5641461006f2befa020201036
Nejc Ribic <nejc.ribic95@gmail.com>
2021-05-06 14:26:47 +0200

    Added URLs combinations

--- spider/boio/boio/spiders/spider.py
+++ spider/boio/boio/spiders/spider.py
@@ -4,7 +4,7 @@ import scrapy
 class SpiderSpider(scrapy.Spider):
     name = 'spider'
     allowed_domains = ['spider.com']
-    start_urls = ['http://spider.com/']
+    start_urls = ['https://prnt.sc/aa0000']
 
     def parse(self, response):
         pass
---
+++ spider/boio/boio/utils/utils.py
@@ -0,0 +1,32 @@
+from string import ascii_lowercase
+
+
+def url_combinations():
+    for c1 in ascii_lowercase:
+        for c2 in ascii_lowercase:
+            for n1 in range(0, 10):
+                for n2 in range(0, 10):
+                    for n3 in range(0, 10):
+                        for n4 in range(0, 10):
+                            write_combination("{}{}{}{}{}{}".format(c1, c2, n1, n2, n3, n4))
+
+
+def log_url(url):
+    filename = 'scraped.log'
+    with open(filename, 'a+') as f:
+        f.write(url + "\n")
+        print(url)
+
+
+def is_already_scraped(url):
+    filename = 'scraped.log'
+    with open(filename) as f:
+        is_scraped = url in f.read()
+        f.close()
+        return is_scraped
+
+
+def write_combination(string):
+    filename = 'urls.log'
+    with open(filename, 'a+') as f:
+        f.write(string + "\n")
